0349ca8388027faa6ac44813888ce01fed6a4a31
KustovYuri <149366447+KustovYuri@users.noreply.github.com>
2024-09-06 12:17:00 +1000

    Fix code style in NewsMainFeaturesUi.kt

--- features/news-main/src/main/java/com/farma/main/NewsMainFeaturesUi.kt
+++ features/news-main/src/main/java/com/farma/main/NewsMainFeaturesUi.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
